a4da478792f8b3a538477b62a963f86263408c38
unknown <sardaralimuradali4@gmail.com>
2022-10-04 01:10:18 -0700

    changing the alert

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -3,8 +3,7 @@ import { useAppContext } from '../context/appContext'
 const Alert = () => {
   const { alertType, alertText } = useAppContext()
   return(
-
-    //   <div className={`alert alert-success`}>bvgcc</div>
+          // This is to chech the changing
          <div className={`alert alert-${alertType}`}>{alertText}</div>
       )
 
